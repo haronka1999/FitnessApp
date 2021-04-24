9e17ba84d91182f755a3d58f93d4687b1cd443ec
Horvath Aron <haronka99@gmail.com>
2021-04-24 12:44:22 +0300

    little improvement on ujkliens xaml and cs files

--- FitnessApp/FitnessApp/FitnessApp/UI/UjKliens.xaml.cs
+++ FitnessApp/FitnessApp/FitnessApp/UI/UjKliens.xaml.cs
@@ -21,6 +21,21 @@ namespace FitnessApp.UI
     /// </summary>
     public partial class UjKliens : System.Windows.Controls.UserControl
     {
+
+        //az infok a form-rol
+        private string name;
+        private string phone;
+        private string email;
+        private string cnp;
+        private string cim;
+        private string berletType;
+        private Image photo;
+        private string comment;
+
+
+        //egyeb valtozok
+        private string date;
+
         public UjKliens()
         {
             InitializeComponent();
@@ -28,15 +43,25 @@ namespace FitnessApp.UI
 
         private void BtnUpload_click(object sender, RoutedEventArgs e)
         {
-            //OpenFileDialog op = new OpenFileDialog();
-            //op.Title = "Select a picture";
-            //op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
-            //  "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
-            //  "Portable Network Graphic (*.png)|*.png";
-            //if (op.ShowDialog() == true)
-            //{
-            //    imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
-            //}
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Válasszon profilképet";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                //idejon a tobbi kod 
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void BtnOk_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancel_click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
