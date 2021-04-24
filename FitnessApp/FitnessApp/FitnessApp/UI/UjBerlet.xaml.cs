197aeb7d4c76b40c380304ba47e35a24c7d027b4
Horvath Aron <haronka99@gmail.com>
2021-04-24 23:23:34 +0300

    ok button functionality added on FoOldal little form validation on UjBerletek

--- FitnessApp/FitnessApp/FitnessApp/UI/UjBerlet.xaml.cs
+++ FitnessApp/FitnessApp/FitnessApp/UI/UjBerlet.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +16,58 @@ using System.Windows.Shapes;
 
 namespace FitnessApp.UI
 {
-    /// <summary>
-    /// Interaction logic for UjBerlet.xaml
-    /// </summary>
+
     public partial class UjBerlet : UserControl
     {
+
+        string text1;
+        string text2;
         public UjBerlet()
         {
             InitializeComponent();
+        }
+
+        private void NumberValidationTextBox_1(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+            text1 = e.Text;
+
+        }
+
+        private void BtnSave_click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.Parse(text1) < 5 || Int32.Parse(text1) > 31)
+            {
+                //error message should be displayed
+            }
+
+            if (Int32.Parse(text2) < 5 || Int32.Parse(text2) > 25)
+            {
+                //error message should be displayed
+            }
+        }
+
+        private void NumberValidationTextBox_2(object sender, TextCompositionEventArgs e)
+        {
+            if(text1 != "")
+            {
+                Regex regex = new Regex("[^0-9]+");
+                e.Handled = regex.IsMatch(e.Text);
+                text2 = e.Text;
+            }
+            else
+            {
+                input_2.IsReadOnly = true;
+
+            }
+
+        }
+
+        private void NumberValidationTextBox_3(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
