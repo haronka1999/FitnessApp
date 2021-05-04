d5100be026f18f55f4937a03aa60baa33e08acd6
HunorZalan <nagyhunorzalan@yahoo.com>
2021-05-04 20:06:34 +0300

    Merge..

--- FitnessApp/FitnessApp/FitnessApp/Utils.cs
+++ FitnessApp/FitnessApp/FitnessApp/Utils.cs
@@ -8,14 +8,16 @@ namespace FitnessApp
 {
     class Utils
     {
-        public static string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\.Net_Project\FitnessApp\FitnessApp\FitnessApp\FitnessApp\Database\db_local.mdf;Integrated Security=True";
-        public static string trash = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/trash.png";
-        public static string edit = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/edit.png";
-        public static string search = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/search.png";
-        public static string upload = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/image_add.png";
-        public static string ok = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/ok.png";
-        public static string save = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/Save.png";
-        public static string email = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/email_send.png";
-        public static string excel = "C:/.Net_Project/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/microsoft_excel.png";
+        public static string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\egyetem\felev2\.net\gyakok\projekt\FitnessApp\FitnessApp\FitnessApp\FitnessApp\Database\db_local.mdf;Integrated Security=True";
+        public static string trash = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/trash.png";
+        public static string edit = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/edit.png";
+        public static string search = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/search.png";
+        public static string upload = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/image_add.png";
+        public static string ok = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/ok.png";
+        public static string save = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/Save.png";
+        public static string email = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/email_send.png";
+        public static string excel = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/microsoft_excel.png";
+        public static string attachment = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/attachment.png";
+        public static string camera = "E:/egyetem/felev2/.net/gyakok/projekt/FitnessApp/FitnessApp/FitnessApp/FitnessApp/UI/camera.png";
     }
 }
